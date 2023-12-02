8ab077acd010ed7c2ab16f94d07740107ca3d0b9
Артур <informal201144@gmai;.com>
2023-12-02 15:06:46 +0500

    fix ads serializers

--- bulletin/serializers.py
+++ bulletin/serializers.py
@@ -3,13 +3,15 @@ from rest_framework import serializers
 from bulletin.models import Ad, Feedback
 
 
-class AdSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Ad
-        fields = '__all__'
-
-
 class FeedbackSerializers(serializers.ModelSerializer):
     class Meta:
         model = Feedback
+        fields = '__all__'
+
+
+class AdSerializers(serializers.ModelSerializer):
+    feedbacks = FeedbackSerializers(source='feedback', many=True, read_only=True)
+
+    class Meta:
+        model = Ad
         fields = '__all__'
